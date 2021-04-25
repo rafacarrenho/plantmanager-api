77ea1a3a04ecba0358284d19c16008ae2572fa38
rafacarrenho <rafael.carrenho@hotmail.com>
2021-04-24 22:45:55 -0300

    first commit

---
+++ pages/api/plants_environments.ts
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import api from "./server.json";
+
+export default (req, res) => {
+  res.status(200).json(api.plants_environments);
+};
